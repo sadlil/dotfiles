ad156ebadb7dfa28c8d126827f893429dbb9e790
Sadlil <sadlil.rhythm@gmail.com>
2020-09-29 12:38:37 +0100

    Update gobuild image

--- dockerfiles/gobuild/check.sh
+++ dockerfiles/gobuild/check.sh
@@ -5,7 +5,7 @@ export RETVAL=0
 
 function go::check_version() {
     version=$(go version)
-    if [[ $version != 'go version go1.12.9 linux/amd64' ]]; then
+    if [[ $version != 'go version go1.15.2 linux/amd64' ]]; then
         echo "go version didn't match"
         exit 1
     fi
